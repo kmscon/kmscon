e3000a996e8d892b93ead131e49a005a2e4d51bf
Vladimir-csp <4061903+Vladimir-csp@users.noreply.github.com>
2025-12-05 03:16:12 +0300

    Portable kmscon-launch-gui.sh
    Fixed printf syntax to be portable and work in dash.
    Got rid of useless `cat`s.
    
    Signed-off-by: Vladimir-csp <4061903+Vladimir-csp@users.noreply.github.com>

--- scripts/kmscon-launch-gui.sh
+++ scripts/kmscon-launch-gui.sh
@@ -6,28 +6,28 @@ if [ "$#" -eq 0 ]; then
 fi
 
 # Store current tty number
-if [ -d /sys/class/tty/tty0 ]; then
-    kms_tty=$(cat /sys/class/tty/tty0/active)
+kms_tty=
+if [ -f /sys/class/tty/tty0/active ]; then
+    read -r kms_tty < /sys/class/tty/tty0/active
 fi
 
 if [ "${TERM_PROGRAM}" != "tmux" ]; then
-    printf "\x1B]setBackground\a"
+    printf '%b' "\033]setBackground\a"
 else
-    printf "\033Ptmux;\033\033]setBackground\a\033\\"
+    printf '%b' "\033Ptmux;\033\033]setBackground\a\033\\"
 fi
 
 "$@"
 
 # If the current tty has changed, wait until the user switches back.
 if [ -n "${kms_tty}" ]; then
-    while [ "$(cat /sys/class/tty/tty0/active)" != "${kms_tty}" ]; do
+    while { read -r check_kms_tty; [ "${check_kms_tty}" != "${kms_tty}" ]; }; do
         sleep 1
     done
 fi
 
 if [ "${TERM_PROGRAM}" != "tmux" ]; then
-    printf "\x1B]setForeground\a"
+    printf '%b' "\033]setForeground\a"
 else
-    printf "\033Ptmux;\033\033]setForeground\a\033\\"
+    printf '%b' "\033Ptmux;\033\033]setForeground\a\033\\"
 fi
-
