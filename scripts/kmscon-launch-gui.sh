96206df10f9c6a23b8d0f7399de1009812f4fca6
Vladimir-csp <4061903+Vladimir-csp@users.noreply.github.com>
2025-12-05 10:22:03 +0300

    inotifywait, shfmt, case
    Use `inotifywait` to monitor foreground console change, if available, otherwise fallback to sleep.
    
    Use `case` for control sequence selectors as it is more compact, elegant, and handy for potential future variant additions.
    
    apply `shfmt -s -sr` formatting.
    
    Signed-off-by: Vladimir-csp <4061903+Vladimir-csp@users.noreply.github.com>

--- scripts/kmscon-launch-gui.sh
+++ scripts/kmscon-launch-gui.sh
@@ -1,35 +1,39 @@
 #!/bin/sh
 
 if [ "$#" -eq 0 ]; then
-    echo "Usage: $0 argument"
-    exit 1
+	echo "Usage: $0 argument"
+	exit 1
 fi
 
 # Store current tty number
 kms_tty=
 active_tty_file=/sys/class/tty/tty0/active
 if [ -f "$active_tty_file" ]; then
-    read -r kms_tty < /sys/class/tty/tty0/active
+	read -r kms_tty < "$active_tty_file"
 fi
 
-if [ "${TERM_PROGRAM}" != "tmux" ]; then
-    printf "\033]setBackground\a"
-else
-    printf "\033Ptmux;\033\033]setBackground\a\033\\"
-fi
+case "${TERM_PROGRAM}" in
+tmux) printf "\033Ptmux;\033\033]setBackground\a\033\\" ;;
+*) printf "\033]setBackground\a" ;;
+esac
 
 "$@"
 
 # If the current tty has changed, wait until the user switches back.
 if [ -n "${kms_tty}" ]; then
-    while { read -r check_kms_tty < "$active_tty_file"; [ "${check_kms_tty}" != "${kms_tty}" ]; }; do
-        sleep 1
-    done
+	read -r check_kms_tty < "$active_tty_file"
+	while [ "${check_kms_tty}" != "${kms_tty}" ]; do
+		# use inotifywait if available, fallback to plain sleep
+		if command -v inotifywait > /dev/null; then
+			inotifywait -qqe modify "$active_tty_file"
+		else
+			sleep 1
+		fi
+		read -r check_kms_tty < "$active_tty_file"
+	done
 fi
 
-if [ "${TERM_PROGRAM}" != "tmux" ]; then
-    printf "\033]setForeground\a"
-else
-    printf "\033Ptmux;\033\033]setForeground\a\033\\"
-fi
-
+case "${TERM_PROGRAM}" in
+tmux) printf "\033Ptmux;\033\033]setForeground\a\033\\" ;;
+*) printf "\033]setForeground\a" ;;
+esac
