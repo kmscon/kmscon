kmscon/kmscon

commit 6964db1483a432012b1e3dcc93604da1b43d24ff

Merge all kmscon_pty_set_xxx() into kmscon_pty_set_conf()
Also simplify error handling

Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>

src/kmscon_terminal.c
@@ -956,29 +956,10 @@ int kmscon_terminal_register(struct kmscon_session **out, struct kmscon_seat *se
 	if (ret)
 		goto err_font;
 
-	kmscon_pty_set_env_reset(term->pty, term->conf->reset_env);
-
-	ret = kmscon_pty_set_term(term->pty, term->conf->term);
+	ret = kmscon_pty_set_conf(term->pty, term->conf->term, "kmscon", term->conf->argv,
+				  kmscon_seat_get_name(seat), term->conf->reset_env);
 	if (ret)
 		goto err_pty;
-
-	ret = kmscon_pty_set_colorterm(term->pty, "kmscon");
-	if (ret)
-		goto err_pty;
-
-	ret = kmscon_pty_set_argv(term->pty, term->conf->argv);
-	if (ret)
-		goto err_pty;
-
-	ret = kmscon_pty_set_seat(term->pty, kmscon_seat_get_name(seat));
-	if (ret)
-		goto err_pty;
-
-	if (vtnr > 0) {
-		ret = kmscon_pty_set_vtnr(term->pty, vtnr);
-		if (ret)
-			goto err_pty;
-	}
 
 	ret = ev_eloop_new_fd(term->eloop, &term->ptyfd, kmscon_pty_get_fd(term->pty), EV_READABLE,
 			      pty_event, term);

src/pty.c
@@ -138,94 +138,33 @@ void kmscon_pty_unref(struct kmscon_pty *pty)
 	free(pty);
 }
 
-int kmscon_pty_set_term(struct kmscon_pty *pty, const char *term)
-{
-	char *t;
-
-	if (!pty || !term)
-		return -EINVAL;
-
-	t = strdup(term);
-	if (!t)
-		return -ENOMEM;
-	free(pty->term);
-	pty->term = t;
-
-	return 0;
-}
-
-int kmscon_pty_set_colorterm(struct kmscon_pty *pty, const char *colorterm)
-{
-	char *t;
-
-	if (!pty || !colorterm)
-		return -EINVAL;
-
-	t = strdup(colorterm);
-	if (!t)
-		return -ENOMEM;
-	free(pty->colorterm);
-	pty->colorterm = t;
-
-	return 0;
-}
-
-int kmscon_pty_set_argv(struct kmscon_pty *pty, char **argv)
-{
-	char **t;
-	int ret;
-
-	if (!pty || !argv || !*argv || !**argv)
-		return -EINVAL;
-
-	ret = shl_dup_array(&t, argv);
-	if (ret)
-		return ret;
-
-	free(pty->argv);
-	pty->argv = t;
-	return 0;
-}
-
-int kmscon_pty_set_seat(struct kmscon_pty *pty, const char *seat)
-{
-	char *t;
-
-	if (!pty || !seat)
-		return -EINVAL;
-
-	t = strdup(seat);
-	if (!t)
-		return -ENOMEM;
-	free(pty->seat);
-	pty->seat = t;
-
-	return 0;
-}
-
-int kmscon_pty_set_vtnr(struct kmscon_pty *pty, unsigned int vtnr)
-{
-	char *t;
-	int ret;
-
-	if (!pty)
-		return -EINVAL;
-
-	ret = asprintf(&t, "%u", vtnr);
-	if (ret < 0)
-		return -ENOMEM;
-	free(pty->vtnr);
-	pty->vtnr = t;
-
-	return 0;
-}
-
-void kmscon_pty_set_env_reset(struct kmscon_pty *pty, bool do_reset)
+int kmscon_pty_set_conf(struct kmscon_pty *pty, const char *term, const char *colorterm,
+			char **argv, const char *seat, bool do_reset)
 {
 	if (!pty)
-		return;
+		return -EINVAL;
 
 	pty->env_reset = do_reset;
+
+	if (term) {
+		pty->term = strdup(term);
+		if (!pty->term)
+			return -ENOMEM;
+	}
+	if (colorterm) {
+		pty->colorterm = strdup(colorterm);
+		if (!pty->colorterm)
+			return -ENOMEM;
+	}
+	if (seat) {
+		pty->seat = strdup(seat);
+		if (!pty->seat)
+			return -ENOMEM;
+	}
+
+	if (argv && *argv && **argv)
+		return shl_dup_array(&pty->argv, argv);
+	return 0;
 }
 
 int kmscon_pty_get_fd(struct kmscon_pty *pty)

src/pty.h
@@ -52,12 +52,8 @@ typedef void (*kmscon_pty_input_cb)(struct kmscon_pty *pty, const char *u8, size
 int kmscon_pty_new(struct kmscon_pty **out, kmscon_pty_input_cb input_cb, void *data);
 void kmscon_pty_ref(struct kmscon_pty *pty);
 void kmscon_pty_unref(struct kmscon_pty *pty);
-int kmscon_pty_set_term(struct kmscon_pty *pty, const char *term);
-int kmscon_pty_set_colorterm(struct kmscon_pty *pty, const char *colorterm);
-int kmscon_pty_set_argv(struct kmscon_pty *pty, char **argv);
-int kmscon_pty_set_seat(struct kmscon_pty *pty, const char *seat);
-int kmscon_pty_set_vtnr(struct kmscon_pty *pty, unsigned int vtnr);
-void kmscon_pty_set_env_reset(struct kmscon_pty *pty, bool do_reset);
+int kmscon_pty_set_conf(struct kmscon_pty *pty, const char *term, const char *colorterm,
+			char **argv, const char *seat, bool do_reset);
 
 int kmscon_pty_get_fd(struct kmscon_pty *pty);
 void kmscon_pty_dispatch(struct kmscon_pty *pty);

commit db4f46fb615d1426a2fc0148532f5735862141f2

Fix XDG_VTNR not set:
There was a regression with commit
6964db1 Merge all kmscon_pty_set_xxx() into kmscon_pty_set_conf()

The XDG_VTNR env variable was no more set in the pty spawned login
shell.

src/kmscon_terminal.c
@@ -916,7 +916,7 @@ int kmscon_terminal_register(struct kmscon_session **out, struct kmscon_seat *se
 		goto err_font;
 
 	ret = kmscon_pty_set_conf(term->pty, term->conf->term, "kmscon", term->conf->argv,
-				  kmscon_seat_get_name(seat), term->conf->reset_env,
+				  kmscon_seat_get_name(seat), vtnr, term->conf->reset_env,
 				  term->conf->backspace_delete);
 	if (ret)
 		goto err_pty;

src/pty.c
@@ -140,8 +140,11 @@ void kmscon_pty_unref(struct kmscon_pty *pty)
 }
 
 int kmscon_pty_set_conf(struct kmscon_pty *pty, const char *term, const char *colorterm,
-			char **argv, const char *seat, bool do_reset, bool backspace)
+			char **argv, const char *seat, unsigned int vtnr, bool do_reset,
+			bool backspace)
 {
+	char *vt;
+
 	if (!pty)
 		return -EINVAL;
 
@@ -163,7 +166,11 @@ int kmscon_pty_set_conf(struct kmscon_pty *pty, const char *term, const char *co
 		if (!pty->seat)
 			return -ENOMEM;
 	}
-
+	if (vtnr) {
+		if (asprintf(&vt, "%u", vtnr) < 0)
+			return -ENOMEM;
+		pty->vtnr = vt;
+	}
 	if (argv && *argv && **argv)
 		return shl_dup_array(&pty->argv, argv);
 	return 0;

src/pty.h
@@ -53,7 +53,8 @@ int kmscon_pty_new(struct kmscon_pty **out, kmscon_pty_input_cb input_cb, void *
 void kmscon_pty_ref(struct kmscon_pty *pty);
 void kmscon_pty_unref(struct kmscon_pty *pty);
 int kmscon_pty_set_conf(struct kmscon_pty *pty, const char *term, const char *colorterm,
-			char **argv, const char *seat, bool do_reset, bool backspace);
+			char **argv, const char *seat, unsigned int vtnr, bool do_reset,
+			bool backspace);
 
 int kmscon_pty_get_fd(struct kmscon_pty *pty);
 void kmscon_pty_dispatch(struct kmscon_pty *pty);