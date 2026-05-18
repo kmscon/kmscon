46ca98486e9dbab16ac7e5c9f038384e60a4e574
Jocelyn Falempe <jfalempe@redhat.com>
2026-05-18 18:44:43 +0200

    Use "kmscon" has default TERM
    Now that kmscon has its own terminfo file, use it by default.
    
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>

--- src/kmscon_conf.c
+++ src/kmscon_conf.c
@@ -99,7 +99,7 @@ static void print_help()
 		"\t                              argv to this process. No more options\n"
 		"\t                              after '--' will be parsed so use it at\n"
 		"\t                              the end of the argument string\n"
-		"\t-t, --term <TERM>           [linux]\n"
+		"\t-t, --term <TERM>           [kmscon]\n"
 		"\t                              Value of the TERM environment variable\n"
 		"\t                              for the child process\n"
 		"\t    --reset-env             [on]\n"
@@ -772,7 +772,7 @@ int kmscon_conf_new(struct conf_ctx **out)
 		CONF_OPTION_STRING(0, "issue-path", &conf->issue_path, "/etc/issue:/etc/issue.d"),
 		CONF_OPTION(0, 'l', "login", &conf_login, aftercheck_login, NULL, file_login,
 			    &conf->login, false),
-		CONF_OPTION_STRING('t', "term", &conf->term, "linux"),
+		CONF_OPTION_STRING('t', "term", &conf->term, "kmscon"),
 		CONF_OPTION_BOOL(0, "reset-env", &conf->reset_env, true),
 		CONF_OPTION_BOOL(0, "backspace-delete", &conf->backspace_delete, true),
 		CONF_OPTION_UINT(0, "sb-size", &conf->sb_size, 1000),
